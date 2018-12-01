0f420d40d4d04523e72c0a95c896743b8ef7e34a
HalilSahin <xelilim@hotmail.com>
2018-12-01 20:13:22 +0300

    Added SqlConnectionStringBuilder and Azure Connection is Done

--- Beltless/Form1.cs
+++ Beltless/Form1.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Data.SqlClient;
 namespace Beltless
 {
     public partial class Form1 : Form
@@ -15,6 +15,30 @@ namespace Beltless
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+            builder.DataSource = "pexax.database.windows.net";
+            builder.UserID = "pexax";
+            builder.Password = "Xelyun12_";
+            builder.InitialCatalog = "Beltlessx";
+            using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+            {
+                connection.Open();
+                string sql = "Select * from Person";
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            textBox1.Text = reader[1].ToString();
+                        }
+                    }
+                }
+            }
         }
     }
 }
