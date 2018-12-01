d58be2d16b1a8cf3c0c6cfd2c9463903d599bfaa
Hüseyin Biçen <huseynbicen@outlook.com>
2018-12-02 01:30:46 +0300

    Koltuklar Eklendi

--- Beltless/Form1.cs
+++ Beltless/Form1.cs
@@ -8,6 +8,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
+
+using System.Threading;
+
 namespace Beltless
 {
     public partial class Form1 : Form
@@ -16,11 +19,33 @@ namespace Beltless
         {
             InitializeComponent();
         }
+        int num = 1;
 
         private void button1_Click(object sender, EventArgs e)
         {
            string k = ConnectionToAzure.ConnectAzure();
             textBox1.Text = k;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Thread th = new Thread(() => showImage(100));
+            th.Start();
+        }
+        
+        public void showImage(int th)
+        {
+            for (int i = 1; i < 11; i++)
+            {
+                string myString = string.Format(@"D:\Documents\Visual Studio 2017\Projets\Beltless\Beltless\Resources\{0}.png", i);
+                Thread.Sleep(th);
+                pictureBox1.ImageLocation = myString;
+            }
         }
     }
 }
