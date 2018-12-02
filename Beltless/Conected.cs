b9232b45a73a99a33390a4f3ba77058ba84fa4ad
Halit MUBAYYİD <32683018+halitmubayid@users.noreply.github.com>
2018-12-02 07:26:53 +0300

    speed değişkenleri Situations'den alıp araba ivmesini, koltuk trak'nın sonunda doğrusal ivmesini, hızısını, dairesl ivmesini, ve kaldırma açısını

---
+++ Beltless/Conected.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beltless
+{
+    class Conected
+    {
+        public static int[] ConnectAzure()
+        {
+            List<int> list = new List<int>();
+            string result = "";
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+            builder.DataSource = "pexax.database.windows.net";
+            builder.UserID = "pexax";
+            builder.Password = "Xelyun12_";
+            builder.InitialCatalog = "Beltlessx";
+            using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+            {
+                connection.Open();
+                string sql = "select Speed from Situations";
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            list.Add(Convert.ToInt32(reader[0]));
+                        }
+                    }
+                }
+
+            }
+            
+            int[] array = new int[list.Count];
+            for (int i = 0; i < list.Count; i++)
+            {
+                array[i] = list[i];
+            }
+            return array;
+        }
+    }
+}
--- Beltless/Form1.cs
+++ Beltless/Form1.cs
@@ -10,6 +10,7 @@ using System.Windows.Forms;
 using System.Data.SqlClient;
 
 using System.Threading;
+using hakaton;
 
 namespace Beltless
 {
@@ -46,6 +47,38 @@ namespace Beltless
                 Thread.Sleep(th);
                 pictureBox1.ImageLocation = myString;
             }
+        }
+
+        public void getSpeed()
+        {
+            int[] d = Conected.ConnectAzure();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            getSpeed();
+            getİnfo();
+        }
+
+        private void getİnfo(int[]array)
+        {
+            LinearMotion car = new LinearMotion();
+            LinearMotion armchairLinearMotion = new LinearMotion();
+            Circular_Motion armchairCircularMotion = new Circular_Motion();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                double acceleration_car = car.acceleration(0, array[i], 1);
+                double speed_armchair = car.final_speed(acceleration_car, 0, 0.15);
+                double circleAccelerationArmchair = armchairCircularMotion.acceleration(speed_armchair, 15);
+                double dete = armchairCircularMotion.angel(speed_armchair, 0.2);
+            }
+            
+            
+            
+            
+
         }
     }
 }
